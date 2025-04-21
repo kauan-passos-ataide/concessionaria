d53a1aa4af6c78780cf88fe85effa039f8c08dad
Kauan P.A <148518067+kauan-passos-ataide@users.noreply.github.com>
2025-04-21 00:16:32 -0300

    remove controller and service of the app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './modules/user/user.module';
@@ -24,7 +22,5 @@ dotenv.config();
     OrderModule,
     AuthModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
